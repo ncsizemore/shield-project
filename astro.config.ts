01a95481558dfefe034d3ffa74c4e7a93a48bb72
ncsizemore <42969232+ncsizemore@users.noreply.github.com>
2025-03-24 11:03:37 -0500

    Update base path for GitHub Pages

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,7 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/shield-project/',
 	site: siteConfig.url,
 	image: {
 		domains: ["webmention.io"],
